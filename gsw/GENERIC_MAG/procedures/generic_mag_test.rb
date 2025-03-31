294e8363c0effdb3fbd3ed6a43e6154207573329
dacarter22 <davidacarter22@gmail.com>
2025-03-31 11:02:31 -0400

    [nasa/nos3#582] Base tests done need to add in data comparison

--- gsw/GENERIC_MAG/procedures/generic_mag_test.rb
+++ gsw/GENERIC_MAG/procedures/generic_mag_test.rb
@@ -2,25 +2,27 @@ require 'cosmos'
 require 'cosmos/script'
 require 'mission_lib.rb'
 
-class LPT < Cosmos::Test
+class MAG_LPT < Cosmos::Test
   def setup
-      enable_TO_and_verify()
+    
   end
 
   def test_lpt
+    start("tests/generic_mag_lpt_test.rb")
   end
 
   def teardown
-      cmd("CFS_RADIO TO_PAUSE_OUTPUT")
+
   end
 end
 
-class CPT < Cosmos::Test
+class MAG_CPT < Cosmos::Test
   def setup
       
   end
 
   def test_cpt
+    start("tests/generic_mag_cpt_test.rb")
   end
 
   def teardown
@@ -31,8 +33,8 @@ end
 class Generic_mag_Test < Cosmos::TestSuite
   def initialize
       super()
-      add_test('CPT')
-      add_test('LPT')
+      add_test('MAG_CPT')
+      add_test('MAG_LPT')
   end
 
   def setup
--- gsw/GENERIC_MAG/procedures/tests/generic_mag_cpt_test.rb
+++ gsw/GENERIC_MAG/procedures/tests/generic_mag_cpt_test.rb
@@ -1,26 +1,154 @@
 require 'cosmos'
 require 'cosmos/script'
 require "cfs_lib.rb"
+#require 'math'
 
+##
+## NOOP
+##
+initial_command_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_MAG GENERIC_MAG_NOOP_CC")
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
 
-# initial_command_count = tlm("SAMPLE SAMPLE_HK_TLM CMD_COUNT")
-# initial_error_count = tlm("SAMPLE SAMPLE_HK_TLM CMD_ERR_COUNT")
-# initial_device_count = tlm("SAMPLE SAMPLE_HK_TLM DEVICE_COUNT")
-# initial_device_error_count = tlm("SAMPLE SAMPLE_HK_TLM DEVICE_ERR_COUNT")
-# cmd("SAMPLE SAMPLE_NOOP_CC")
-# wait_check("SAMPLE SAMPLE_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM DEVICE_COUNT == #{initial_device_count}", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+sleep(5)
+
+##
+## Successful Disable
+##
+initial_command_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_MAG GENERIC_MAG_DISABLE_CC")
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ENABLED == 'DISABLED'", 30)
+
+sleep(5)
+
+##
+## Failed Disable (doubled)
+##
+initial_command_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_MAG GENERIC_MAG_DISABLE_CC")
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT > #{initial_device_error_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ENABLED == 'DISABLED'", 30)
+
+sleep(5)
+
+##
+## HK without Device
+##
+initial_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_MAG GENERIC_MAG_REQ_HK")
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+
+sleep(5)
+
+##
+## Data without Device
+##
+initial_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_MAG GENERIC_MAG_REQ_DATA")
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+
+sleep(5)
+
+##
+## Successful Enable
+##
+initial_command_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_MAG GENERIC_MAG_ENABLE_CC")
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ENABLED == 'ENABLED'", 30)
+
+sleep(5)
+
+##
+## Failed Enable (doubled)
+##
+initial_command_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_MAG GENERIC_MAG_ENABLE_CC")
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT > #{initial_device_error_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ENABLED == 'ENABLED'", 30)
+
+sleep(5)
+
+##
+## Housekeeping w/ Device
+##
+initial_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_MAG GENERIC_MAG_REQ_HK")
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+
+sleep(5)
+
+# ##
+# ## Data w/ Device
+# ##
+# initial_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT")
+# initial_device_error_count = tlm("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT")
+
+# cmd("GENERIC_MAG GENERIC_MAG_REQ_DATA")
+
+# svb0 = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA SVB_0")
+# svb1 = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA SVB_1")
+# svb2 = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA SVB_2")
+
+# fss_alpha = tlm("GENERIC_MAG GENERIC_MAG_DATA_TLM GENERIC_MAG_ALPHA")
+# fss_beta = tlm("GENERIC_MAG GENERIC_MAG_DATA_TLM GENERIC_MAG_BETA")
+# fss_error = tlm("GENERIC_MAG GENERIC_MAG_DATA_TLM GENERIC_MAG_ERROR_CODE")
+
+# truth_42_alpha = -Math.atan2(svb2, svb0)
+# truth_42_beta = Math.atan2(svb1, svb0)
+
+# truth_42_alpha_diff = (fss_alpha - truth_42_alpha).abs()
+# truth_42_beta_diff = (fss_beta - truth_42_beta).abs()
+# diff_margin = 0.025
+
+# wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+# wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+# if fss_error == 0
+#   wait_check_expression("truth_42_alpha_diff <= diff_margin # #{truth_42_alpha_diff} <= #{diff_margin}", 15)
+
+#   wait_check_expression("truth_42_beta_diff <= diff_margin # #{truth_42_beta_diff} <= #{diff_margin}", 15)
+# end
 
 # sleep(5)
 
-# initial_command_count = tlm("SAMPLE SAMPLE_HK_TLM CMD_COUNT")
-# initial_error_count = tlm("SAMPLE SAMPLE_HK_TLM CMD_ERR_COUNT")
-# initial_device_count = tlm("SAMPLE SAMPLE_HK_TLM DEVICE_COUNT")
-# initial_device_error_count = tlm("SAMPLE SAMPLE_HK_TLM DEVICE_ERR_COUNT")
-# cmd("SAMPLE SAMPLE_RST_COUNTERS_CC")
-# wait_check("SAMPLE SAMPLE_HK_TLM CMD_COUNT == 0", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM CMD_ERR_COUNT == 0", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM DEVICE_COUNT == 0", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM DEVICE_ERR_COUNT == 0", 30)
+##
+## Reset Counters
+##
+cmd("GENERIC_MAG GENERIC_MAG_DISABLE_CC") # disable to be able to properly check that device count gets reset
+cmd("GENERIC_MAG GENERIC_MAG_RST_COUNTERS_CC")
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_COUNT == 0", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM CMD_ERR_COUNT == 0", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_COUNT == 0", 30)
+wait_check("GENERIC_MAG GENERIC_MAG_HK_TLM DEVICE_ERR_COUNT == 0", 30)
+
+##
+## Reenable so test can be run multiple times
+##
+cmd("GENERIC_MAG GENERIC_MAG_ENABLE_CC")
