199d1b05ca923fd45921be6a785b46f37b88cf9e
Kevin Bruce <77984549+kevincbruce@users.noreply.github.com>
2025-04-10 15:56:21 -0400

    [nasa/nos3#582] mag system tests (#11)
    * [nasa/nos3#582] Base tests done need to add in data comparison
    * [nasa/nos#582] adjusted diff margin and multiplier for unit accuracy
    * [nasa/nos3#582] Updates to use check instead of wait_check in scripts;
    ---------
    Co-authored-by: dacarter22 <davidacarter22@gmail.com>
    Co-authored-by: Lucas, John P. <John.P.Lucas@nasa.gov>

--- gsw/GENERIC_MAG/procedures/generic_mag_test.rb
+++ gsw/GENERIC_MAG/procedures/generic_mag_test.rb
@@ -1,43 +1,51 @@
 require 'cosmos'
 require 'cosmos/script'
-require 'mission_lib.rb'
+require 'generic_mag_lib.rb'
 
-class LPT < Cosmos::Test
+class GENERIC_MAG_Functional_Test < Cosmos::Test
   def setup
-      enable_TO_and_verify()
+    safe_generic_mag()
   end
 
-  def test_lpt
+  def test_application
+      start("tests/generic_mag_app_test.rb")
+  end
+
+  def test_device
+    start("tests/generic_mag_device_test.rb")
   end
 
   def teardown
-      cmd("CFS_RADIO TO_PAUSE_OUTPUT")
+    safe_generic_mag()
   end
 end
 
-class CPT < Cosmos::Test
-  def setup
-      
+class GENERIC_MAG_Automated_Scenario_Test < Cosmos::Test
+  def setup 
+    safe_generic_mag()
   end
 
-  def test_cpt
+  def test_AST
+      start("tests/generic_mag_ast_test.rb")
   end
 
   def teardown
-
+    safe_generic_mag()
   end
 end
 
 class Generic_mag_Test < Cosmos::TestSuite
   def initialize
       super()
-      add_test('CPT')
-      add_test('LPT')
+      add_test('GENERIC_MAG_Functional_Test')
+      add_test('GENERIC_MAG_Automated_Scenario_Test')
   end
 
   def setup
+    safe_generic_mag()
   end
   
   def teardown
+    safe_generic_mag()
   end
 end
