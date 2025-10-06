b036d77949f3697e3a8d3799c2e348fe6caf404e
zlynch2 <zachary.j.lynch@nasa.gov>
2025-10-06 19:30:04 +0000

    [nasa/nos3#735] updating mag for 4.0

--- fsw/fprime/mag_src/Generic_mag.cpp
+++ fsw/fprime/mag_src/Generic_mag.cpp
@@ -70,7 +70,8 @@ namespace Components {
   void Generic_mag :: NOOP_cmdHandler(FwOpcodeType opCode, U32 cmdSeq){
     HkTelemetryPkt.CommandCount++;
 
-    this->log_ACTIVITY_HI_TELEM("NOOP command success!");
+    Fw::LogStringArg log_msg("NOOP command success!");
+    this->log_ACTIVITY_HI_TELEM(log_msg);
     this->tlmWrite_CommandCount(HkTelemetryPkt.CommandCount);
     this->tlmWrite_DeviceEnabled(get_active_state(HkTelemetryPkt.DeviceEnabled));
     this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
@@ -95,18 +96,21 @@ namespace Components {
       {
         HkTelemetryPkt.DeviceEnabled = GENERIC_MAG_DEVICE_ENABLED;
         HkTelemetryPkt.DeviceCount++;
-        this->log_ACTIVITY_HI_TELEM("Enable command success!");
+        Fw::LogStringArg log_msg("Enable command success!");
+        this->log_ACTIVITY_HI_TELEM(log_msg);
       }
       else
       {
         HkTelemetryPkt.DeviceErrorCount++;
-        this->log_ACTIVITY_HI_TELEM("Enable command failed to init SPI!");
+        Fw::LogStringArg log_msg("Enable command failed to init SPI!");
+        this->log_ACTIVITY_HI_TELEM(log_msg);
       }
     }
     else
     {
       HkTelemetryPkt.CommandErrorCount++;
-      this->log_ACTIVITY_HI_TELEM("Enable failed, already Enabled!");
+      Fw::LogStringArg log_msg("Enable failed, already Enabled!");
+      this->log_ACTIVITY_HI_TELEM(log_msg);
     }
 
     this->tlmWrite_CommandCount(HkTelemetryPkt.CommandCount);
@@ -130,18 +134,21 @@ namespace Components {
       {
         HkTelemetryPkt.DeviceEnabled = GENERIC_MAG_DEVICE_DISABLED;
         HkTelemetryPkt.DeviceCount++;
-        this->log_ACTIVITY_HI_TELEM("Disable command success!");
+        Fw::LogStringArg log_msg("Disable command success!");
+        this->log_ACTIVITY_HI_TELEM(log_msg);
       }
       else
       {
         HkTelemetryPkt.DeviceErrorCount++;
-        this->log_ACTIVITY_HI_TELEM("Disable command failed to close SPI!");
+        Fw::LogStringArg log_msg("Disable command failed to close SPI!");
+        this->log_ACTIVITY_HI_TELEM(log_msg);
       }
     }
     else
     {
       HkTelemetryPkt.CommandErrorCount++;
-      this->log_ACTIVITY_HI_TELEM("Disable failed, already Disabled!");
+      Fw::LogStringArg log_msg("Disable failed, already Disabled!");
+      this->log_ACTIVITY_HI_TELEM(log_msg);
     }
 
     this->tlmWrite_CommandCount(HkTelemetryPkt.CommandCount);
@@ -168,11 +175,13 @@ namespace Components {
       this->tlmWrite_DeviceErrorCount(HkTelemetryPkt.DeviceErrorCount);
       this->tlmWrite_DeviceEnabled(get_active_state(HkTelemetryPkt.DeviceEnabled));
 
-      this->log_ACTIVITY_HI_TELEM("Requested Housekeeping!");
+      Fw::LogStringArg log_msg("Requested Housekeeping!");
+      this->log_ACTIVITY_HI_TELEM(log_msg);
     }
     else
     {
-      this->log_ACTIVITY_HI_TELEM("HK Failed, Device Disabled!");
+      Fw::LogStringArg log_msg("HK Failed, Device Disabled!");
+      this->log_ACTIVITY_HI_TELEM(log_msg);
     }   
 
     this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
@@ -190,18 +199,21 @@ namespace Components {
       if (status == OS_SUCCESS)
       {
         HkTelemetryPkt.DeviceCount++;
-        this->log_ACTIVITY_HI_TELEM("RequestData command success\n");
+        Fw::LogStringArg log_msg("RequestData command success\n");
+        this->log_ACTIVITY_HI_TELEM(log_msg);
       }
       else
       {
         HkTelemetryPkt.DeviceErrorCount++;
-        this->log_ACTIVITY_HI_TELEM("RequestData command failed!\n");
+        Fw::LogStringArg log_msg("RequestData command failed!\n");
+        this->log_ACTIVITY_HI_TELEM(log_msg);
       }
     }
     else
     {
       HkTelemetryPkt.CommandErrorCount++;
-      this->log_ACTIVITY_HI_TELEM("RequestData command failed, device disabled!\n");
+      Fw::LogStringArg log_msg("RequestData command failed, device disabled!\n");
+      this->log_ACTIVITY_HI_TELEM(log_msg);
     }
     
     this->tlmWrite_MagneticIntensityX(Generic_magData.MagneticIntensityX);
@@ -251,7 +263,8 @@ namespace Components {
     HkTelemetryPkt.DeviceCount = 0;
     HkTelemetryPkt.DeviceErrorCount = 0;
 
-    this->log_ACTIVITY_HI_TELEM("Reset Counters command successful!");
+    Fw::LogStringArg log_msg("Reset Counters command successful!");
+    this->log_ACTIVITY_HI_TELEM(log_msg);
     this->tlmWrite_CommandCount(HkTelemetryPkt.CommandCount);
     this->tlmWrite_CommandErrorCount(HkTelemetryPkt.CommandErrorCount);
     this->tlmWrite_DeviceCount(HkTelemetryPkt.DeviceCount);
